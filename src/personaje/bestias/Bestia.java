bd14e193d11f9eb7853935ad15bc9396f9334068
Manuel Rojas <manuerjs70@gmail.com>
2021-06-22 22:28:53 -0600

    Cree la clase padre, personaje y las clases hijas heroe y bestia

---
+++ src/personaje/bestias/Bestia.java
@@ -0,0 +1,11 @@
+package src.personaje.bestias;
+
+import src.personaje.Personaje;
+
+public class Bestia extends Personaje{
+
+    public Bestia(String nombre, int vida, int armadura) {
+        super(nombre, vida, armadura);
+    }
+    
+}
---
+++ src/personaje/heroes/Heroe.java
@@ -0,0 +1,12 @@
+package src.personaje.heroes;
+
+import src.personaje.Personaje;
+
+public class Heroe extends Personaje {
+
+    public Heroe(String nombre, int vida, int armadura) {
+        super(nombre, vida, armadura);
+        //TODO Auto-generated constructor stub
+    }
+    
+}
