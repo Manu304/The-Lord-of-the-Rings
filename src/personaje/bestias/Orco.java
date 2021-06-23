696d1668abc9d5fc644466121787a8651ccf39f1
Manuel Rojas <manuerjs70@gmail.com>
2021-06-22 22:32:27 -0600

    Cree las clases que heredan de Bestia

---
+++ src/personaje/bestias/Orco.java
@@ -0,0 +1,10 @@
+package src.personaje.bestias;
+
+public class Orco extends Bestia {
+
+    public Orco(String nombre, int vida, int armadura) {
+        super(nombre, vida, armadura);
+        //TODO Auto-generated constructor stub
+    }
+    
+}
---
+++ src/personaje/bestias/Trasgo.java
@@ -0,0 +1,10 @@
+package src.personaje.bestias;
+
+public class Trasgo extends Bestia {
+
+    public Trasgo(String nombre, int vida, int armadura) {
+        super(nombre, vida, armadura);
+        //TODO Auto-generated constructor stub
+    }
+    
+}
