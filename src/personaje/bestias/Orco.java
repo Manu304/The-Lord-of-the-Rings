20f41bf5f86bc46bcd0b7139e4e9192cf58a9fa3
Manuel Rojas <manuerjs70@gmail.com>
2021-06-22 23:36:34 -0600

    Agregue vida correspondiente a las bestias

--- src/personaje/bestias/Orco.java
+++ src/personaje/bestias/Orco.java
@@ -2,9 +2,8 @@ package src.personaje.bestias;
 
 public class Orco extends Bestia {
 
-    public Orco(String nombre, int vida, int armadura) {
-        super(nombre, vida, armadura);
-        //TODO Auto-generated constructor stub
+    public Orco(String nombre, int vida) {
+        super(nombre, 300);
     }
     
 }
--- src/personaje/bestias/Trasgo.java
+++ src/personaje/bestias/Trasgo.java
@@ -3,8 +3,7 @@ package src.personaje.bestias;
 public class Trasgo extends Bestia {
 
     public Trasgo(String nombre, int vida) {
-        super(nombre, vida);
-        //TODO Auto-generated constructor stub
+        super(nombre, 325);
     }
     
 }
